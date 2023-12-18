1a8e2260a7d2147cc8ec9ef21596d152c3b3fd5a
aicha-azr <aichaazeroual03@gmail.com>
2023-12-18 14:39:04 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Home.css";
+const Home = ()=>{
+    return(
+        <>
+        <div className='container'>
+    <div className='title'>Welcome to Dev quiz</div>
+   
+  
+</div>
+        </>
+    )
+}
+export default Home;
+/* 
+     <img  src="https://via.placeholder.com/280x194" />
+    <img  src="https://via.placeholder.com/280x194" />
+    <img src="../assets/gg.gif" />
+*/
