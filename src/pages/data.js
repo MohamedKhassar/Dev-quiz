50f8f4dae1ce8edc335bb8ddcb2c0b3e15d8db6b
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-19 14:29:20 +0100

    Update quiz questions and add major selection

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,45 +1,244 @@
-const data=[
-    {
-        q:"",
-        a:[
-            {
-                id:1,
-                t:"",
-                answer:true
-            },
-            {
-                id:2,
-                t:"",
-                answer:false
-            },
-            {
-                id:3,
-                t:"",
-                answer:false
-            }
-
-        ]
-    },
-    {
-        q:"",
-        a:[
-            {
-                id:1,
-                t:"",
-                answer:true
-            },
-            {
-                id:2,
-                t:"",
-                answer:false
-            },
-            {
-                id:3,
-                t:"",
-                answer:false
-            }
-
-        ],
-        cat:""
-    }
-]
+const questions= [
+        {
+          "id": 1,
+          "question": "What does CRUD stand for?",
+          "options": [
+            "Create, Read, Update, Delete",
+            "Code, Run, Understand, Deploy",
+            "Compile, Run, Update, Debug",
+            "Copy, Remove, Update, Deploy"
+          ],
+          "correctAnswer": "Create, Read, Update, Delete",
+          "category": "Full-stack"
+        },
+        {
+          "id": 2,
+          "question": "Which programming language is commonly used for server-side development in a full-stack application?",
+          "options": [
+            "JavaScript",
+            "Python",
+            "Java",
+            "Node.js"
+          ],
+          "correctAnswer": "Node.js",
+          "category": "Full-stack"
+        },
+        {
+          "id": 3,
+          "question": "What is the purpose of Git in the development process?",
+          "options": [
+            "Database management",
+            "Version control",
+            "Front-end styling",
+            "Database querying"
+          ],
+          "correctAnswer": "Version control",
+          "category": "Full-stack"
+        },
+        {
+          "id": 4,
+          "question": "Which database type is commonly used in full-stack development?",
+          "options": [
+            "Relational database",
+            "NoSQL database",
+            "Graph database",
+            "In-memory database"
+          ],
+          "correctAnswer": "Relational database",
+          "category": "Full-stack"
+        },
+        {
+          "id": 5,
+          "question": "What is the purpose of npm in Node.js development?",
+          "options": [
+            "Node Package Manager",
+            "Network Protocol Module",
+            "Node Programming Module",
+            "Node Project Manager"
+          ],
+          "correctAnswer": "Node Package Manager",
+          "category": "Full-stack"
+        },
+        {
+          "id": 6,
+          "question": "What is the significance of RESTful APIs in web development?",
+          "options": [
+            "Real-time communication",
+            "Request and State Transfer",
+            "Responsive and Scalable Technologies",
+            "Representational State Transfer"
+          ],
+          "correctAnswer": "Representational State Transfer",
+          "category": "Full-stack"
+        },
+        {
+          "id": 7,
+          "question": "In JavaScript, what does the 'this' keyword refer to?",
+          "options": [
+            "The current function",
+            "The global object",
+            "The calling function",
+            "The parent object"
+          ],
+          "correctAnswer": "The calling function",
+          "category": "Full-stack"
+        },
+        {
+          "id": 8,
+          "question": "What is the purpose of the CSS 'box-sizing' property?",
+          "options": [
+            "Define the size of a box",
+            "Specify the box model to be used",
+            "Set the box shadow",
+            "Adjust the box layout"
+          ],
+          "correctAnswer": "Specify the box model to be used",
+          "category": "Frontend"
+        },
+        {
+          "id": 9,
+          "question": "Which HTTP status code indicates a successful request in web development?",
+          "options": [
+            "200 OK",
+            "404 Not Found",
+            "500 Internal Server Error",
+            "302 Found"
+          ],
+          "correctAnswer": "200 OK",
+          "category": "Frontend"
+        },
+        {
+          "id": 10,
+          "question": "What is the purpose of the 'SQL JOIN' operation in databases?",
+          "options": [
+            "Inserting data into a table",
+            "Retrieving data from multiple tables",
+            "Updating existing records",
+            "Deleting records from a table"
+          ],
+          "correctAnswer": "Retrieving data from multiple tables",
+          "category": "Full-stack"
+        },
+        {
+          "id": 11,
+          "question": "Which tag is used to define an ordered list in HTML?",
+          "options": [
+            "<ul>",
+            "<ol>",
+            "<li>",
+            "<dl>"
+          ],
+          "correctAnswer": "<ol>",
+          "category": "Frontend"
+        },
+        {
+          "id": 12,
+          "question": "In CSS, what property is used to set the background color of an element?",
+          "options": [
+            "background-color",
+            "color",
+            "text-color",
+            "foreground-color"
+          ],
+          "correctAnswer": "background-color",
+          "category": "Frontend"
+        },
+        {
+          "id": 13,
+          "question": "What is the purpose of the 'Django' framework in web development?",
+          "options": [
+            "JavaScript framework",
+            "CSS preprocessor",
+            "Server-side framework for Python",
+            "Frontend framework"
+          ],
+          "correctAnswer": "Server-side framework for Python",
+          "category": "Backend"
+        },
+        {
+          "id": 14,
+          "question": "Which of the following is a type of NoSQL database?",
+          "options": [
+            "MySQL",
+            "MongoDB",
+            "PostgreSQL",
+            "SQLite"
+          ],
+          "correctAnswer": "MongoDB",
+          "category": "Backend"
+        },
+        {
+          "id": 15,
+          "question": "What is the purpose of the 'Express' framework in Node.js development?",
+          "options": [
+            "Frontend development",
+            "Database management",
+            "Server-side routing and middleware",
+            "CSS preprocessing"
+          ],
+          "correctAnswer": "Server-side routing and middleware",
+          "category": "Backend"
+        },
+        {
+          "id": 16,
+          "question": "What is the role of the 'viewport' meta tag in responsive web design?",
+          "options": [
+            "Define the page title",
+            "Set the character encoding",
+            "Configure the browser's viewport",
+            "Include external stylesheets"
+          ],
+          "correctAnswer": "Configure the browser's viewport",
+          "category": "Frontend"
+        },
+        {
+          "id": 17,
+          "question": "Which CSS property is used to control the layout of a web page?",
+          "options": [
+            "font-size",
+            "margin",
+            "padding",
+            "display"
+          ],
+          "correctAnswer": "display",
+          "category": "Frontend"
+        },
+        {
+          "id": 18,
+          "question": "What is the purpose of the 'bcrypt' library in backend development?",
+          "options": [
+            "Frontend styling",
+            "Password hashing",
+            "Asynchronous programming",
+            "Database querying"
+          ],
+          "correctAnswer": "Password hashing",
+          "category": "Backend"
+        },
+        {
+          "id": 19,
+          "question": "Which HTTP method is typically used for retrieving data from the server?",
+          "options": [
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"
+          ],
+          "correctAnswer": "GET",
+          "category": "Backend"
+        },
+        {
+          "id": 20,
+          "question": "What is the purpose of the 'webpack' module bundler in frontend development?",
+          "options": [
+            "Database management",
+            "Code compilation",
+            "Server-side routing",
+            "Asynchronous programming"
+          ],
+          "correctAnswer": "Code compilation",
+          "category": "Frontend"
+        }
+      ]
+    
+  export default questions
--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import "./start.css"
 import { useNavigate } from "react-router-dom"
+import questions from "../data"
 export default function Start() {
     const [major,setM]=useState("")
+    const question=questions.filter(q=>q.category===major)
+    console.log(question);
     const nav=useNavigate()
     return (
         <div className="container">
@@ -11,9 +14,9 @@ export default function Start() {
                 <div className="animation">
                 </div>
                 <div className="code-sym">
-                    <button className="major-btn" onClick={(e)=>setM(e.target.value)} value="Full-Stack">Full-Stack</button>
-                    <button className="major-btn" onClick={(e)=>setM(e.target.value)} value="Front-End">Front-End</button>
-                    <button className="major-btn" onClick={(e)=>setM(e.target.value)} value="Back-End">Back-End</button>
+                    <button className="major-btn" onClick={(e)=>setM(e.target.value)} value="Full-stack">Full-Stack</button>
+                    <button className="major-btn" onClick={(e)=>setM(e.target.value)} value="Frontend">Front-End</button>
+                    <button className="major-btn" onClick={(e)=>setM(e.target.value)} value="Backend">Back-End</button>
                     <button className="start-btn" onClick={()=>nav("/quiz")}>Start Quiz</button>
                 </div>
             </div>
