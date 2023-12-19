903348a3bc192205fc4009dc9f7e4a288c909379
aicha-azr <aichaazeroual03@gmail.com>
2023-12-19 16:51:28 +0100

    Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Start from './pages/start/Start'
+import Quiz from './pages/Quiz'
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <Routes>
       <Route path='/' exact element={<Home/>}/>
       <Route path='/start' exact element={<Start />}/>
-      
+      <Route path='/quiz' exact element={<Quiz />}/>
     </Routes>
     </BrowserRouter>
   )
