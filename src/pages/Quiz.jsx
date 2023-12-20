5126dec9fa538b550e2b3682ad429a6cc6343c27
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-20 10:14:53 +0100

    Update quiz page with new questions and local storage.

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import  { useState } from 'react';
 import './Quiz.css';
+import questions from './data';
+import { useLocalStorage } from './useLocalStorage';
 function Quiz(){
+    const {getItem}=useLocalStorage("major")
+    const question = questions.filter(q => q.category === getItem())
+    console.log(question);
+    let [i, setI] = useState(0);
+    const [next, setN] = useState(question[i])
+
+    
+
+    const nexQuestion = () => {
+        if (i < question.length - 1) {
+            setI(pre => pre += 1)
+            setN(question[i])
+        } else {
+            setI(0)
+            alert("hi")
+            
+        }
+
+        console.log(next);
+    }
     return(
         <>
         <div className="all">
         <h1 className="cat">Full-Stack<hr />
         </h1>
         
-        <p className="qst">What does "Full-Stack" refer to in the context of web development?</p>
+        <p className="qst">What does &quot;Full-Stack&quot; refer to in the context of web development?</p>
         <p className='number'>1/10</p>
         <div className='options'>
+        {}
         <button  id="0">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</button>
-        <button id="1">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</button>
-        <button id="2">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</button>
-        <button id="3">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</button>
         <button className='next'>Next question</button>
         </div>
         </div>
--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -2,25 +2,14 @@ import { useState } from "react"
 import "./start.css"
 import { useNavigate } from "react-router-dom"
 import questions from "../data"
+import { useLocalStorage } from "../useLocalStorage"
 export default function Start() {
     const [major, setM] = useState("")
-    const question = questions.filter(q => q.category === major)
-    let [i, setI] = useState(0);
-    const [next, setN] = useState(question[i])
+    
+    const {setItem}=useLocalStorage("major")
+    
 
-
-    const nexQuestion = () => {
-        if (i < question.length - 1) {
-            setI(pre => pre += 1)
-            setN(question[i])
-        } else {
-            setI(0)
-            alert("hi")
-            
-        }
-
-        console.log(next);
-    }
+    
 
     const nav = useNavigate()
     return (
@@ -33,7 +22,7 @@ export default function Start() {
                     <button className="major-btn" onClick={(e) => setM(e.target.value)} value="Full-stack">Full-Stack</button>
                     <button className="major-btn" onClick={(e) => setM(e.target.value)} value="Frontend">Front-End</button>
                     <button className="major-btn" onClick={(e) => setM(e.target.value)} value="Backend">Back-End</button>
-                    <button className="start-btn" onClick={() => nav("/quiz")}>Start Quiz</button>
+                    <button className="start-btn" onClick={() => {setItem(major) ;nav("/quiz")}}>Start Quiz</button>
                 </div>
             </div>
         </div>
