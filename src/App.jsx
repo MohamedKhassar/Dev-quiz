9e2516eb5b851bb9081e770b6f6c4a677ca7d85a
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-21 10:00:50 +0100

    Revert "Remove unnecessary files and code"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home/Home'
 import Start from './pages/start/Start'
-import Quiz from './pages/Quiz'
+import Quiz from './pages/quiz/Quiz'
+import Form from './pages/form/form'
+
 
 function App() {
 
@@ -10,6 +12,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' exact element={<Home/>}/>
+      <Route path='/form' exact element={<Form />}/>
       <Route path='/start' exact element={<Start />}/>
       <Route path='/quiz' exact element={<Quiz />}/>
     </Routes>
