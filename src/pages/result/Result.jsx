116c9341b86fcc6e335117f976c49efefac982ec
khassarMohamed <mohamedkhassar775@gmail.com>
2023-12-22 15:29:18 +0100

    Update Lottie animation and add a close button.

--- src/pages/result/Result.jsx
+++ src/pages/result/Result.jsx
@@ -1,36 +1,51 @@
 import './result.css';
 import { useLocalStorage } from "../useLocalStorage";
 import { useNavigate } from 'react-router-dom';
-import animationData from '../../assets/lottie/congrats.json'
+import success from '../../assets/lottie/congrats.json'
+import fail from '../../assets/lottie/fail.json'
 import Lottie from 'lottie-react';
+import { useEffect, useState } from 'react';
 
 export default function Result() {
 
   const { getItem } = useLocalStorage("user")
-  const nav = useNavigate()
+  const [isHide,setIsHide]=useState(false)
+  const nav=useNavigate()
 
-  const close = () => {
-    localStorage.clear()
-    nav("/")
-  }
 
-  return (
-    <div className="wrapped">
-      <div style={{ position:"absolute" }}>
-        <Lottie loop={false} width={"90%"} animationData={animationData}   />
-        </div>
-      <div className="list">
-        <h1 className="title">Final Result</h1>
-        <div className="list1">
-          <div className="firstN"><p>First name: </p><p>{getItem().name}</p></div>
-          <div className="lastN"><p>Last name: </p><p>{getItem().last}</p></div>
-          <div className="major"><p >Major: </p><p >{getItem().major}</p></div>
-          <div className="score"><p>Score :</p><p>{getItem().score}%</p></div>
+useEffect(()=>{
+  const timeOute=setTimeout(()=>{
 
-        </div>
-        <button className="close" onClick={close}>close</button>
-      </div>
+    if (!isHide) {
+      setIsHide(true)
+    }
+    return ()=>clearInterval(timeOute)
+  },1500)
+},[isHide])
 
-    </div>
-  )
+const close=()=>{
+  nav("/")
+  localStorage.clear()
 }
+
+    return (
+      <div className="wrapped">
+        <div style={{ position: "absolute" }}>
+
+          {!isHide&&<Lottie loop={false} animationData={getItem().score > 50 ? success : fail} />}
+        </div>
+        <div className="list">
+          <h1 className="title">Final Result</h1>
+          <div className="list1">
+            <div className="firstN"><p>First name: </p><p>{getItem().name}</p></div>
+            <div className="lastN"><p>Last name: </p><p>{getItem().last}</p></div>
+            <div className="major"><p >Major: </p><p >{getItem().major}</p></div>
+            <div className="score"><p>Score :</p><p>{getItem().score}%</p></div>
+
+          </div>
+          <button className="close" onClick={close}>close</button>
+        </div>
+
+      </div>
+    )
+  }
